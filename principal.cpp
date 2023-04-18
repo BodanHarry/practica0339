eac62a9440d3cd937b090ba97a0887821781d4bc
maurauca <maura.quintanilla15903@est.uca.edu.ni>
2023-04-18 17:23:33 -0600

    maura

--- principal.cpp
+++ principal.cpp
@@ -3,10 +3,12 @@ using namespace std;
 #include "harry.cpp"
 #include "bradly.cpp"
 #include "diego.cpp"
+#include "maura.cpp"
 
 int main() {
     harry();
     bradly();
     diego();
+    maura();
     return 0;
 }
