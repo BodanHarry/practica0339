1f208baa26f8645a740ccc81e231c30f84143839
DBeewms <diego.mora16063@est.uca.edu.ni>
2023-04-18 17:01:03 -0600

    Diego

--- bradly.cpp
+++ bradly.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h> 
 
 void bradly(){
-    printf("Me llamo bradly");
+    printf("Me llamo bradly\n");
 }
--- principal.cpp
+++ principal.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 #include "harry.cpp"
 #include "bradly.cpp"
+#include "diego.cpp"
 
 int main() {
     harry();
     bradly();
+    diego();
     return 0;
 }
